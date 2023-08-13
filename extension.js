35d93684428d8a87d25a8ecfe827b164f046d8f4
edsand <eduardsand.po@gmail.com>
2023-08-12 18:08:39 -0700

    first commit

---
+++ extension.js
@@ -0,0 +1,21 @@
+const { Gtk } = imports.gi;
+
+const gtk_major_version = Gtk.get_major_version();
+const gtk_minor_version = Gtk.get_minor_version();
+log(`GTK version is ${gtk_major_version}.${gtk_minor_version}`);
+
+class Extension {
+    constructor() {
+    }
+
+    enable() {
+    }
+
+    disable() {
+    }
+}
+
+function init() {
+	log('custom extension');
+    return new Extension();
+}
